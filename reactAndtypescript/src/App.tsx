375f330314a2a4c1606548a45ebeacec8a1cdf52
rodolfo valtierra <233263@gmail.com>
2025-05-19 18:07:02 +0000

    finished react&typescript

---
+++ reactAndtypescript/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Hello from './Hello'
+
+function App() {
+
+  return (
+    <>
+      <Hello name="David" enthusiasmLevel={2}></Hello>
+    </>
+  )
+}
+
+export default App
