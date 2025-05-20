9d8079341765856d6d508a19513bd84172663b6c
rodolfo valtierra <233263@gmail.com>
2025-05-20 18:05:28 +0000

    Common use case state

--- Hooks/src/App.tsx
+++ Hooks/src/App.tsx
@@ -1,20 +1,23 @@
 import { useState } from 'react'
 
 function App() {
-  const [message, setMessage] = useState('')
-
-  function handleClick () {
-    setMessage('I\'m hele!')
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  function loadData() {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setData([1, 2, 3, 4]);
+    }, 3000);
   }
 
-
-
   return (
-      <div className="container">
-        <button onClick={handleClick}>Display</button>
-        <p>{message}</p>
-      </div>
-  )
+    <>
+      {loading && <p>Loading...</p>}
+      <pre>{JSON.stringify(data, null, ' ')}</pre>
+      <button onClick={loadData}>Load Data</button>
+    </>
+  );
 }
 
 export default App
