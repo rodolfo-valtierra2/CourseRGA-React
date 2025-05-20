96a48021b0e2e659dd1e6961821720e04d53e2aa
rodolfo valtierra <233263@gmail.com>
2025-05-20 19:25:11 +0000

    Conditional operator

--- Hooks/src/App.tsx
+++ Hooks/src/App.tsx
@@ -2,25 +2,20 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-
   const handleClick = () => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
   };
 
-  let menu;
-  if (isOpen) {
-    menu = (
-      <ul>
-        <li>Edit</li>
-        <li>Remove</li>
-        <li>Archive</li>
-      </ul>
-    );
-  }
   return (
     <div>
       <button onClick={handleClick}>Actions</button>
-      {menu}
+      {isOpen ? (
+        <ul>
+          <li>Edit</li>
+          <li>Remove</li>
+          <li>Archive</li>
+        </ul>
+      ) : null}
     </div>
   );
 }
